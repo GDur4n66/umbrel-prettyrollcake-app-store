5d77caf268af675bd0466fb4bd69abf6d649304c
tanakei <100189072+GDur4n66@users.noreply.github.com>
2022-10-30 11:24:52 +0900

    Add files via upload

---
+++ prettyrollcake-bitcoin-csig/exports.sh
@@ -0,0 +1,68 @@
+export APP_BITCOIN_CUSTOMSIGNET_IP="10.21.95.10"
+export APP_BITCOIN_CUSTOMSIGNET_NODE_IP="10.21.95.20"
+
+export APP_BITCOIN_CUSTOMSIGNET_DATA_DIR="${EXPORTS_APP_DIR}/data/bitcoin"
+export APP_BITCOIN_CUSTOMSIGNET_RPC_PORT="38332"
+export APP_BITCOIN_CUSTOMSIGNET_P2P_PORT="38333"
+export APP_BITCOIN_CUSTOMSIGNET_TOR_PORT="38334"
+export APP_BITCOIN_CUSTOMSIGNET_ZMQ_RAWBLOCK_PORT="60002"
+export APP_BITCOIN_CUSTOMSIGNET_ZMQ_RAWTX_PORT="60003"
+export APP_BITCOIN_CUSTOMSIGNET_ZMQ_HASHBLOCK_PORT="60004"
+export APP_BITCOIN_CUSTOMSIGNET_ZMQ_SEQUENCE_PORT="60005"
+
+BITCOIN_CHAIN="signet"
+BITCOIN_ENV_FILE="${EXPORTS_APP_DIR}/.env"
+
+if [[ ! -f "${BITCOIN_ENV_FILE}" ]]; then
+
+	if [[ -z ${BITCOIN_RPC_USER+x} ]] || [[ -z ${BITCOIN_RPC_PASS+x} ]] || [[ -z ${BITCOIN_RPC_AUTH+x} ]]; then
+		BITCOIN_RPC_USER="umbrel"
+		BITCOIN_RPC_DETAILS=$("${EXPORTS_APP_DIR}/scripts/rpcauth.py" "${BITCOIN_RPC_USER}")
+		BITCOIN_RPC_PASS=$(echo "$BITCOIN_RPC_DETAILS" | tail -1)
+		BITCOIN_RPC_AUTH=$(echo "$BITCOIN_RPC_DETAILS" | head -2 | tail -1 | sed -e "s/^rpcauth=//")
+	fi
+
+#	echo "export APP_BITCOIN_CUSTOMSIGNET_NETWORK=signet"				    >  "${BITCOIN_ENV_FILE}"
+	echo "export APP_BITCOIN_CUSTOMSIGNET_RPC_USER='${BITCOIN_RPC_USER}'"	>> "${BITCOIN_ENV_FILE}"
+	echo "export APP_BITCOIN_CUSTOMSIGNET_RPC_PASS='${BITCOIN_RPC_PASS}'"	>> "${BITCOIN_ENV_FILE}"
+	echo "export APP_BITCOIN_CUSTOMSIGNET_RPC_AUTH='${BITCOIN_RPC_AUTH}'"	>> "${BITCOIN_ENV_FILE}"
+	
+fi
+
+. "${BITCOIN_ENV_FILE}"
+
+BIN_ARGS=()
+BIN_ARGS+=( "-chain=${BITCOIN_CHAIN}" )
+BIN_ARGS+=( "-proxy=${TOR_PROXY_IP}:${TOR_PROXY_PORT}" )
+BIN_ARGS+=( "-listen" )
+BIN_ARGS+=( "-bind=0.0.0.0:${APP_BITCOIN_CUSTOMSIGNET_TOR_PORT}=onion" )
+BIN_ARGS+=( "-bind=${APP_BITCOIN_CUSTOMSIGNET_NODE_IP}" )
+BIN_ARGS+=( "-port=${APP_BITCOIN_CUSTOMSIGNET_P2P_PORT}" )
+BIN_ARGS+=( "-rpcport=${APP_BITCOIN_CUSTOMSIGNET_RPC_PORT}" )
+BIN_ARGS+=( "-rpcbind=${APP_BITCOIN_CUSTOMSIGNET_NODE_IP}" )
+BIN_ARGS+=( "-rpcbind=127.0.0.1" )
+BIN_ARGS+=( "-rpcallowip=${NETWORK_IP}/16" )
+BIN_ARGS+=( "-rpcallowip=127.0.0.1" )
+BIN_ARGS+=( "-rpcauth=\"${APP_BITCOIN_CUSTOMSIGNET_RPC_AUTH}\"" )
+BIN_ARGS+=( "-zmqpubrawblock=tcp://0.0.0.0:${APP_BITCOIN_CUSTOMSIGNET_ZMQ_RAWBLOCK_PORT}" )
+BIN_ARGS+=( "-zmqpubrawtx=tcp://0.0.0.0:${APP_BITCOIN_CUSTOMSIGNET_ZMQ_RAWTX_PORT}" )
+BIN_ARGS+=( "-zmqpubhashblock=tcp://0.0.0.0:${APP_BITCOIN_CUSTOMSIGNET_ZMQ_HASHBLOCK_PORT}" )
+BIN_ARGS+=( "-zmqpubsequence=tcp://0.0.0.0:${APP_BITCOIN_CUSTOMSIGNET_ZMQ_SEQUENCE_PORT}" )
+BIN_ARGS+=( "-txindex=1" )
+BIN_ARGS+=( "-blockfilterindex=1" )
+BIN_ARGS+=( "-peerbloomfilters=1" )
+BIN_ARGS+=( "-peerblockfilters=1" )
+BIN_ARGS+=( "-rpcworkqueue=128" )
+BIN_ARGS+=( "-signetchallenge=512102e605bd40b535bf1819179c622c0ce036b6f8b1b251288a91401c2805e65de71b51ae" )
+
+
+export APP_BITCOIN_CUSTOMSIGNET_COMMAND=$(IFS=" "; echo "${BIN_ARGS[@]}")
+
+# echo "${APP_BITCOIN_CUSTOMSIGNET_COMMAND}"
+
+rpc_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-rpc/hostname"
+p2p_hidden_service_file="${EXPORTS_TOR_DATA_DIR}/app-${EXPORTS_APP_ID}-p2p/hostname"
+export APP_BITCOIN_CUSTOMSIGNET_RPC_HIDDEN_SERVICE="$(cat "${rpc_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
+export APP_BITCOIN_CUSTOMSIGNET_P2P_HIDDEN_SERVICE="$(cat "${p2p_hidden_service_file}" 2>/dev/null || echo "notyetset.onion")"
+
+
